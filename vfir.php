e15238215405cdb78e0ae0dbc27092e8fc4da47d
dishashete <dishashete09@gmail.com>
2023-12-25 13:55:04 +0530

    done

---
+++ vfir.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sample1";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$cid = $_POST['id'];
+$status = $_POST['status'];
+
+$sqlquery = "UPDATE  fir set status='$status' where id='$cid'";
+
+if ($conn->query($sqlquery) == TRUE) {
+  echo "<script> alert('Verfication Sucessfull');window.location='firshow.php';</script>";
+} else {
+  echo $conn->error;
+}
